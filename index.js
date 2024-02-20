302ca23dad27d5438fced7935d3f49d677541aa4
yangmeng <291840280@qq.com>
2024-02-20 14:45:34 +0800

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: yangmeng yangmeng@staff.hexun.com
+ * @Date: 2024-02-20 14:39:06
+ * @LastEditors: yangmeng yangmeng@staff.hexun.com
+ * @LastEditTime: 2024-02-20 14:39:06
+ * @FilePath: /test/index.js
+ */
+const a = 1
