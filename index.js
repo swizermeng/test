849b2200b2677b76ac1f01e90e7a84a31ea925a7
yangmeng <291840280@qq.com>
2024-02-20 14:46:56 +0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /*
  * @Author: yangmeng yangmeng@staff.hexun.com
  * @Date: 2024-02-20 14:39:06
- * @LastEditors: yangmeng yangmeng@staff.hexun.com
- * @LastEditTime: 2024-02-20 14:39:06
+ * @LastEditors: yangmeng 291840280@qq.com
+ * @LastEditTime: 2024-02-20 14:46:44
  * @FilePath: /test/index.js
  */
 const a = 1
+const b = 2
